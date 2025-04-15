647e1be3cf95846203b89148e3b0ee3d2ea356fe
ehgjs0415 <ehgjs0415@naver.com>
2025-04-15 21:00:07 +0900

    4.15공부

---
+++ js/searchhw1.js
@@ -0,0 +1,15 @@
+document.getElementById("search_button_msg").addEventListener('click', search_message);
+
+function search_message(){
+    alert("첫번째 검색을 수행합니다.");
+}
+function search_message(){
+    alert("두번째 검색을 수행합니다.");
+}
+
+function search_message(){
+    let a = '검색을 수행합니다.'
+    alert(a);
+}
+
+// 4주차 실습과제
