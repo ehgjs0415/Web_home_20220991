282dfb63efe635581cebbe727e639929ffe171d3
admin <cdhgod0@nate.com>
2025-04-30 10:49:22 +0900

    4.30 실습

--- js/pop_up.js
+++ js/pop_up.js
@@ -1,7 +1,7 @@
 // var close_time; // 시간 정보
 // var close_time2 = 10; // 10초 설정
 // clearTimeout(close_time); // 재호출 정지
-// close_time= setTimeout("close_window()", 10000);
+// close_time= setTimeout("close_window()", 50000);
 
 // // 1/1000 초 지정, 바로 시작
 // show_time(); // 실시간 시간 보여주기
@@ -16,10 +16,40 @@
 //     window.close(); // 윈도우 닫기
 // } // 윈도우 창 2개가 닫힘, --> 파일을 2개로 쪼개야함popup2.js로 만들어서 사용. 윈도우 창만 닫기(팝업창만)
     
+function setCookie(name, value, expiredays) {
+    var date = new Date();
+    date.setDate(date.getDate() + expiredays);
+    document.cookie = escape(name) + "=" + escape(value) + "; expires=" + date.toUTCString() + "; path=/";
+}
 
+function getCookie(name) {
+    var cookie = document.cookie;
+    console.log("쿠키를 요청합니다.");
+    if (cookie != "") {
+        var cookie_array = cookie.split("; ");
+        for ( var index in cookie_array) {
+            var cookie_name = cookie_array[index].split("=");
+            if (cookie_name[0] == "popupYN") {
+                return cookie_name[1];
+            }
+        }
+    }
+    return ;
+}
 
-function pop_up(){
-    window.open("../popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
+function closePopup() {
+    if (document.getElementById('check_popup').value) {
+        setCookie("popupYN", "N", 1);
+        console.log("쿠키를 설정합니다.");
+        self.close();
+    }
+}
+
+function pop_up() {
+    var cookieCheck = getCookie("popupYN");
+        if (cookieCheck != "N",1){
+        window.open("../popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
+    }
 }
 
 // 1초 마다 갱신을 수행하는 날짜 함수
