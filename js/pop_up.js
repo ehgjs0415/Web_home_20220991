50c59dd05202585bf8ae7be918c30a0b11a5cfe9
ehgjs0415 <ehgjs0415@naver.com>
2025-06-06 23:48:52 +0900

    6.6최종

--- js/pop_up.js
+++ js/pop_up.js
@@ -57,39 +57,10 @@ function show_clock(){
     setTimeout(show_clock, 1000) // 1초마다 갱신
 }
 
-// function show_clock(){
-//     let currentDate= new Date(); // 현재시스템날짜객체생성
-//    let divClock= document.getElementById('divClock');
-//     let msg = "현재시간: ";
-//     if(currentDate.getHours()>12){  // 12시보다크면오후아니면오전
-//    msg += "오후";
-//     msg += currentDate.getHours()-12+"시";
-//     }
-//     else {
-//     msg += "오전";
-//     msg += currentDate.getHours()+"시";
-//     }
-//     msg += currentDate.getMinutes()+"분";
-//     msg += currentDate.getSeconds()+"초";
-//     divClock.innerText= msg;
-//     if (currentDate.getMinutes()>58) {    //정각1분전빨강색출력
-//    divClock.style.color="red";
-//     }
-//     setTimeout(show_clock, 1000);  //1초마다갱신
-//    }
-
-
-
 //이미지 호버 기능
 function over(obj){
     obj.src="image/logo.png";
 }
-
-// es6버전이후 function으로 안해도 됨 --> 화살표로 표현가능
-// const over = (obj) => {
-//     obj.src = "image/logo.png";
-// }
-
 
 function out(obj){
     obj.src="image/logo2.jfif";
