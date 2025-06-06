831e08004c0ebb78119b81305c7e0a25dbac6ce3
ehgjs0415 <ehgjs0415@naver.com>
2025-06-06 23:59:28 +0900

    6.6최종

--- js/pop_up.js
+++ js/pop_up.js
@@ -29,8 +29,8 @@ function closePopup() {
 
 function pop_up() {
     var cookieCheck = getCookie("popupYN");
-        if (cookieCheck != "N",1){
-        window.open("../popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
+        if (cookieCheck !== "N"){
+        window.open("popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
     }
 }
 
