07c7bc85154b5ce8905002babca28d30cd0935f7
ehgjs0415 <ehgjs0415@naver.com>
2025-06-07 00:57:56 +0900

    6.7

--- js/pop_up.js
+++ js/pop_up.js
@@ -30,7 +30,7 @@ function closePopup() {
 function pop_up() {
     var cookieCheck = getCookie("popupYN");
         if (cookieCheck !== "N"){
-        window.open("popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
+        window.open("/javawebHW/popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
     }
 }
 
